5efdfe83d65ea2c1a60dab585da9c66379d132cd
Timur <hasan13@me.com>
2017-11-05 17:46:03 +0300

    Cleaning
    Cleaned App2.js
    Deleted App.js
    Renamed App2.js -> App.js
    Cleaned index.js

--- src/App.js
+++ src/App.js
@@ -1,166 +1,12 @@
 import React, { Component } from 'react';
-import ReactDOM from "react-dom";
 
-var tasksDone = 0;
-
-class TaskMenu extends Component {
-
-    constructor(props) {
-      super(props);
-      this.state = {
-        tasks: [["Нажми чтобы добавить", false, "control"]],
-        active: false
-      };
-
-      this.addTask = this.addTask.bind(this);
-      this.removeTask = this.removeTask.bind(this);
-      this.up = this.up.bind(this);
-      this.done = this.done.bind(this);
-      this.sort = this.sort.bind(this);
-    }
-
-    addTask(e){
-
-       // console.log(this.state.tasks[0]); // VOT ONO
-        
-      var task = [prompt('What to write ?', ""), true];
-      var index = e.target.getAttribute("data-index");
-
-      var newTasks = [];
-
-       if (task !== "") { //если ввод не пустой то добавляем
-       // newTasks.push(["Нажми чтобы добавить", false, "control"]);
-       console.log("old tasks are: " + [this.state.tasks][0][0]);
-        newTasks.push([this.state.tasks]);
-        console.log("pushed old tasks");
-        newTasks.push([task]);
-        console.log("pushed new task: " + [task]);
-
-
-        this.setState({
-           tasks: newTasks
-        });
-     
-
-        }
-        /*
-        if (task !== "") { //если ввод не пустой то добавляем
-        this.setState({
-            tasks: 
-            +index=== this.state.tasks.length-1 
-            ? [...this.state.tasks, ["Нажми чтобы добавитьф", false, "control"]]     
-            : [...this.state.tasks]
-        });
-        }
-        */
-
-      
-
-    
-
-    };
-
-    removeTask(e) {
-
-        var index = e.target.getAttribute("data-index");
-        console.log(this.state.tasks[index][1]);
-
-        if (this.state.tasks[index][1] === true) {
-            this.state.tasks.splice(index,1);     
-            this.setState([this.state.tasks[index][1] === false]);
-        };
-    };
-
-    up(e) {
-        var index = e.target.getAttribute("data-index");
-        if (this.state.tasks[index][1] === true) {
-            var elem = this.state.tasks[index];
-            this.state.tasks.splice(index,1);
-            this.state.tasks.unshift(elem);
-            this.setState([this.state.active=true]);
-        };
-    }
-
-    done(e) {
-        var index = e.target.getAttribute("data-index");
-        if (this.state.tasks[index][1] === true) {
-            if (this.state.tasks[index][2] !== "done") {
-                this.setState([this.state.tasks[index][2] = "done"]);
-                tasksDone +=1;
-            };
-        };
-    }
-
-    sort(e) {
-        var index = e.target.getAttribute("data-index");
-        if (this.state.tasks[index][2] === "control") {
-           this.setState(this.state.tasks.sort());
-        };
-    }
-
-    render() {
-      return (
-          
-        <ul>
-          {this.state.tasks.map((task, i)=>
-            <li key={i}> 
-
-
-                <button style={ this.state.tasks[i][2] === "done" 
-                ? styles.buttonStyleDone 
-                : styles.buttonStyleNormal}         onClick={this.addTask}         data-index={i}>{task[0]}</button> 
-
-                { //Done Button
-                    this.state.tasks[i][2] === "control" ? null : 
-                    <button style={styles.buttonStyleOk} onClick={this.done}        data-index={i}>✔</button> 
-                }
-
-
-                { //Remove Button
-                    this.state.tasks[i][2] === "control" ? null : 
-                    <button style={styles.buttonStyleX}  onClick={this.removeTask}  data-index={i}>✖</button> 
-                }
-               
-               { //Up Button
-                    this.state.tasks[i][2] === "control" ? null : 
-                    <button style={styles.buttonStyleUp} onClick={this.up}          data-index={i}>▲</button> 
-                }
- 
-
-                { //Sort Button
-                    this.state.tasks[i][2] !== "control" ? null : 
-                    <button style={styles.buttonStyleUp} onClick={this.sort}        data-index={i}>SORT</button>
-                }
-
-                { //Counter
-                    this.state.tasks[i][2] !== "control" ? null : 
-                    <h1> Counter: {tasksDone}</h1>
-                }
-                 
-  
-            </li>
-            
-          )}
-        </ul>
-
-       
-        
-      )
-
-    }
-
-
-  }
-
-
-
-  const styles = {
+const styles = {
     buttonStyleOk: {
-    backgroundColor: "green"
+        backgroundColor: "green"
     },
     buttonStyleX: {
         backgroundColor: "red"
-        },
+    },
     buttonStyleUp: {
         backgroundColor: "cyan"
     },
@@ -168,13 +14,109 @@ class TaskMenu extends Component {
         backgroundColor: "#EFFBFB"
     },
     buttonStyleDone: {
-        backgroundColor: "gray"
+        backgroundColor: "gray",
+        textDecorationLine: 'line-through', 
+        textDecorationStyle: 'solid'
     }
-    }
-  
-  ReactDOM.render(
-      <TaskMenu />,
-      document.getElementById("root")
-  );
+}
 
-    export default TaskMenu;
+class TaskMenu extends Component {
+        constructor(props) {
+          super(props);
+          this.state = {active: true, myTasks: this.props.tasks}
+          this.addNewTask = this.addNewTask.bind(this);
+          this.up = this.up.bind(this);
+          this.removeTask = this.removeTask.bind(this);
+          this.sortTasks = this.sortTasks.bind(this);
+          };
+
+        addNewTask() {
+            let task = prompt('What to write ?', "");
+            task = (task !== null ? task : "")
+            if (task !== "") {
+                this.state.myTasks.push(task);
+            } else {
+                alert("Empty Task Name!!!");
+            }
+            this.setState({active : !this.state.active});
+        }
+
+        removeTask(e) {
+            this.setState(this.state.myTasks.splice(e.target.getAttribute("data-index2"), 1));
+        }
+
+        sortTasks() {
+            this.setState(this.state.myTasks.sort());
+        }
+
+        up(e) {
+            var index = e.target.getAttribute("data-index3");
+                var elem = this.state.myTasks[index];
+                this.state.myTasks.splice(index,1);
+                this.state.myTasks.unshift(elem);
+                this.setState({active : !this.state.active});
+            
+        }
+
+        render() {//<AddButton /> 
+              return <div> 
+                   <button onClick={this.addNewTask}>Click on me to add a new Task! </button>
+                   <button style={styles.buttonStyleUp} onClick={this.sortTasks}>SORT</button>
+                   <ul>
+                   {this.state.myTasks.map((task, i)=>
+                   <li key={i}>
+                       {this.state.myTasks[i]}
+                        <TaskButton data-index={i} taskText={this.state.myTasks[i]}/>
+                        <button data-index2={i} style={styles.buttonStyleX} onClick={this.removeTask}>✖</button>
+                        <button data-index3={i} style={styles.buttonStyleUp} onClick={this.up}>▲</button> 
+                        
+                   </li>
+                   )}
+                   </ul>
+                   </div>
+                }
+        }         
+
+
+class TaskButton extends Component {
+    constructor(props) {
+        super(props);
+        this.changeTask = this.changeTask.bind(this);
+        this.done = this.done.bind(this);
+        this.state = {name: this.props.taskText, done: false};
+    };
+
+    changeTask() {
+        let text = prompt('What to write ?', "");
+          if (this.state.done === true) {
+              this.setState({done: false});
+          }
+        this.setState({name: text})
+    }
+
+    done() {
+        this.setState({done: !this.state.done});
+    }
+
+    render() { 
+        var buttonStyle = ( this.state.done === true 
+            ? styles.buttonStyleDone 
+            : styles.buttonStyleNormal)
+
+        if (this.state.name !== "") {
+        return (
+        <div>
+            <button onClick={this.changeTask} style={buttonStyle}> {this.props.taskText} </button>
+
+            <button style={styles.buttonStyleOk} onClick={this.done}>✔</button>
+            
+        </div>
+        )
+        } else { return null}
+
+    }
+}
+
+
+
+  export default TaskMenu;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import TaskMenu from './App2';
-//import Todo from './Test';
-//import './index.css';
+import TaskMenu from './App';
 
-//import PropTypes from 'prop-types';
-
-//var createReactClass = require('create-react-class');
-
- //var tl = ["Task 1", "Task 2", "task3"];
  var tl = [];
 
 ReactDOM.render(
   // <div> <TaskMenu tasks = {tl} /><TaskMenu tasks = {tl}/> </div> , document.getElementById('root')
    <div> <TaskMenu tasks={tl}/>  </div> , document.getElementById('root')
+   
 );
 
 export default TaskMenu;
